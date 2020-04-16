25b30a1203b76de7ff2eb54b50cbfe6859903004
robmcelhinney <robmcelhinney@hotmail.com>
2020-04-16 23:45:08 +0100

    Add reload button to show default word buttons.
    Add reload button that sets the min and max numbers of words back to the
    default values.
    Create .gitattributes to hide css as main language on github.
    
    (cherry picked from commit 41359a382c019b721065722056dcd997469d22b4)

--- src/Components/WordButtons.js
+++ src/Components/WordButtons.js
@@ -51,19 +51,37 @@ const WordButtons = (props) => {
 		setMinNumberWords(minNumberWords-1);
 	}
 
+	const reloadMinMax = () => {
+		setMinNumberWords(myConstClass.MIN_NUM_WORDS);
+		setMaxNumberWords(myConstClass.MAX_NUM_WORDS);
+	}
+
 	return (
 		<div>
 			<div className="max-w-full">
 				{mapButtons()}
 			</div>
 			<div>
-				<button className={'word-count-button font-bold py-2 px-4 border hover:border-purple-500 rounded my-2 mx-2 .w-2' + (minNumberWords <= 1 ? " opacity-50 cursor-not-allowed" : "")}
+				<button className={"word-count-button font-bold py-2 " +
+						"px-4 border hover:border-purple-500 rounded " +
+						"my-2 mx-2 +.w-2" + (minNumberWords <= 1 ? 
+						" opacity-50 cursor-not-allowed" : "")}
 						onClick={decreaseMin}
 						disabled={minNumberWords <= 1}>
 					-
 				</button>
+				<button className={"word-count-button font-bold py-2 px-4 " +
+						"border hover:border-purple-500 rounded my-2 mx-2 " +
+						".w-2" + (minNumberWords === 
+						myConstClass.MIN_NUM_WORDS && maxNumberWords === 
+						myConstClass.MAX_NUM_WORDS ? 
+						" opacity-50 cursor-not-allowed" : "")}
+						onClick={reloadMinMax}>
+					↻
+				</button>
 				<button className="word-count-button font-bold py-2 px-4 
-						border hover:border-purple-500 rounded my-2 mx-2 .w-2"
+						border hover:border-purple-500 rounded my-2 mx-2 
+						.w-2"
 						onClick={increaseMax}>
 					+
 				</button>
